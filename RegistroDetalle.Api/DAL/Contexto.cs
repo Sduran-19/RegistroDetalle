d182f1f651aaeeadf9d9743901abb96d6b2f8106
Samuel Durán García <samuel_duran1@ucne.edu.do>
2024-02-26 02:13:15 -0400

    agregando contexto

---
+++ RegistroDetalle.Api/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using RegistroDetalle.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroDetalle.Api.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prioridades>? Prioridades { get; set; }
+
+        public DbSet<Clientes>? Clientes { get; set; }
+
+        public DbSet<Tickets>? Tickets { get; set; }
+
+        public DbSet<Sistemas>? Sistemas { get; set; }
+
+        public DbSet<TicketsDetalle>? TicketsDetalle { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    }
+}
